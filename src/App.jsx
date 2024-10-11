bd064ca6d816ef8139da6557eebb52077d8eee65
Nabeel Talib <engrnabeel.talib@gmail.com>
2024-10-11 23:23:32 +0500

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,47 @@
-// import { useState } from 'react'
-// import Card from './components/Card'
-// import './App.css'
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <h1>Olx Card</h1>
-//       <Card />
-
-//     </>
-//   )
-// }
-
-// export default App
-
-
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Olxcard from './components/Olxcard'
 
 const App = () => {
-  // const [showCard , setShowCard] = useState(false);
+  const [showCard , setshowCard] = useState(false);
+  const [products, setProducts] = useState(null);
+
+  useEffect(()=>{
+         
+           fetch('https://dummyjson.com/products')
+            .then(res => res.json())
+            .then(res => setProducts(res.products))
+            .catch(err => console.log(err))
+              
+          
+        } , [])
+        console.log("data: ", products);
 
   return (
     <>
     <div className="py-5 text-center">
-    <h1 className='display-1 fw-bold'>Olx Cards</h1>
+    <h1 className='display-1 fw-bold'>Onilne Shopping</h1>
     </div>
     <div className="container">
-      <div className="row">
-      <div className="col-12">
-      {/* price, title, location, day */}
-    <Olxcard image="https://via.placeholder.com/200" price="Rs 430,000" title="IPhone 16 Pro Max 256 On Easy Installments" location="DHA Phase 1, Lahore" day="2 days ago" />
+      <div className="row g-5">
+      <div className="col-12 text-center">
+
+      <button className='btn btn-primary' onClick={()=> setshowCard(!showCard)}>{showCard ? 'Hide Cards' : 'Show Cards'}</button> <br /><br />
       </div>
+      
+
+                  
+                  {showCard &&  products.map((item, index) =>( <div className="col-4"><Olxcard key={index} 
+                  image={item.images}
+                  category={item.category}
+                  price={item.price}
+                  title={item.title}
+                  description={item.description}
+                  warrantyInfo={item.warrantyInformation} /> </div>))
+                  
+                }
+                    
+    
+      
     </div>
     </div>
     
--- src/components/Olxcard.jsx
+++ src/components/Olxcard.jsx
@@ -1,15 +1,18 @@
 
 
-const Olxcard = ({image, price, title, location, day }) => {
+const Olxcard = ({image, price, title, category, description, warrantyInfo }) => {
   return (
 <>
-<div className="card" style={{ width: '25rem' }}>
+<div className="card border border-secondary position-relative">
+<span className="position-absolute fs-5 top-0 start-50 translate-middle badge rounded-pill bg-dark">
+    {category}
+  </span>
   <img src={image} className="card-img-top" style={{ height: '250px' }} alt="" />
-  <div className="card-body">
-    <h5 className="card-title">{price}</h5>
-    <p className="card-text pb-2">{title}</p>
-    <p className="mb-0">{location}</p>
-    <p className="mb-0">{day}</p>
+  <div className="card-body border-top border-2 border-warning">
+    <h5 className="card-title">$ {price}</h5>
+    <p className="card-text fw-bold">{title}</p>
+    <p>{description}</p>
+    <p className="mb-0">{warrantyInfo}</p>
   </div>
 </div>
 </>
